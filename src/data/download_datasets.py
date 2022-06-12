e5e4a2058dae68579bee2be987d99a33d58bf88f
Amir Ahmed <amirtheahmed@gmail.com>
2022-06-12 12:05:30 +0300

    added download progress bar

--- src/data/download_datasets.py
+++ src/data/download_datasets.py
@@ -22,16 +22,23 @@ def download_datasets():
         # create response object
         r = requests.get(link, stream=True)
 
-        # download started
-        with open(f'../../data/{file_name}', 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024 * 1024):
-                if chunk:
-                    f.write(chunk)
+        total_size_in_bytes = int(r.headers.get('content-length', 0))
+        block_size = 1024
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+        with open(f'../../data/{file_name}', 'wb') as file:
+            for data in r.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+
+        progress_bar.close()
+        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+            print("ERROR, something went wrong")
 
         print("%s downloaded!\n" % file_name)
 
     print("All files downloaded!")
     return
+
 
 
 def unzip_files():
