ce347fc7a07262a37c1d85f04390347c057f147c
Amir Ahmed <amirtheahmed@gmail.com>
2022-06-12 14:31:51 +0300

    separated dataset download and extract function intoseparate file

--- src/data/extract_datasets.py
+++ src/data/extract_datasets.py
@@ -1,4 +1,6 @@
 import requests
+from zipfile import ZipFile
+from pathlib import Path
 from tqdm import tqdm
 
 DATASETS = [
@@ -39,6 +41,22 @@ def download_datasets():
     return
 
 
+
+def unzip_files():
+    for link in DATASETS:
+        file_name = link.split('/')[-1]
+        # opening the zip file in READ mode
+        with ZipFile(f'../../data/{file_name}', 'r') as zip:
+            # printing all the contents of the zip file
+            zip.printdir()
+
+            # extracting all the files
+            print('Extracting all the files now...')
+            zip.extractall(path='../../')
+            print('Done!')
+
+
 if __name__ == "__main__":
-    # Path('../../data/').mkdir(parents=True, exist_ok=True)
+    Path('../../data/').mkdir(parents=True, exist_ok=True)
     download_datasets()
+    unzip_files()
